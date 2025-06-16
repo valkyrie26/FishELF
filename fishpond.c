1b4c37fb39ff6933c030570314608911ad48fa2e
valkyrie26 <itzzps.official@gmail.com>
2025-06-16 09:56:05 -0400

    Initial commit: Added fishpond.c with Magikarp function

---
+++ fishpond.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void magikarp() {
+    printf("You caught a Magikarp! It’s splashing wildly...\n");
+}
+
+int main() {
+    printf("Welcome to FishELF! Use your tools to explore these waters...\n");
+    return 0;
+}
